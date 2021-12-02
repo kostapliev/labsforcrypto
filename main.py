b04a0b8096d2db6b401282d9c579957dc3d39a42
Konstantin Pliev <plievkd@mail.ru>
2021-12-02 13:13:42 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,62 +1,104 @@
+# функция уменьшает число до тех пор пока одно из них не станет нулем
+# практически для этого используется цикл
+def evklidsimply(a,b):
+    while a != 0 and b != 0:
+        if a >= b:
+            a %= b
+        else:
+            b %= a
+    return a or b
+
+#  функция расширенного евклида
+# ax + by = gcd(a,b)
+# алгоритм находит нод и его линейное представление
+
+def evklid_extended(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        div, x, y = evklid_extended(b % a, a)
+    return (div, y - (b // a) * x, x)
+
+# функция бинарного евклида
+def binary_evklid(a,b):
+    g = 1  # переменная для подсчета
+    # согласно условиям и пунктам задачи мы все делаем
+    # по пунктам
+    while(a % 2 == 0 and b % 2 == 0):
+        a = a/2
+        b = b/2
+        g = 2*g
+    u,v = a,b
+    while u != 0:
+        if u % 2 == 0:
+            u = u/2
+        if v % 2 == 0:
+            v = v/2
+        if u >= v:
+            u = u - v
+        else:
+            v = v - u
+    d = g*v
+    return d
+
+# функция расширенного бинарного евклида
+def evklid_binary_extended(a, b):
+    g = 1 # переменная для подсчетов
+    # выполняем все согласно алгоритму
+    # объяснять даже не надо все по пунктам расписано в условии задачи
+    while (a % 2 == 0 and b % 2 == 0):
+        a = a / 2
+        b = b / 2
+        g = 2 * g
+    u = a
+    v = b
+    A = 1
+    B = 0
+    C = 0
+    D = 1
+    while u != 0:
+        if u % 2 == 0:
+            u = u/2
+            if A % 2 == 0 and B % 2 ==0:
+                A = A/2
+                B = B/2
+            else:
+                A = (A+b)/2
+                B = (B-a)/2
+        if v % 2 == 0:
+            v = v / 2
+            if C%2==0 and D%2==0:
+                C = C/2
+                D = D/2
+            else:
+                C = (C+b)/2
+                D = (D-a)/2
+        if u>=v:
+            u = u - v
+            A = A - C
+            B = B - D
+        else:
+            v = v - u
+            C = C - A
+            D = D - B
+    d = g*v
+    x = C
+    y = D
+    return (d,x,y)
+
 def main():
-    # создаем алфавит
-    dicts = {"а": 1, "б": 2, "в": 3, "г": 4, "д": 5, "е": 6, "ё": 7, "ж": 8, "з": 9, "и": 10, "й": 11, "к": 12, "л": 13,
-             "м": 14, "н": 15, "о": 16, "п": 17,
-             "р": 18, "с": 19, "т": 20, "у": 21, "ф": 22, "х": 23, "ц": 24, "ч": 25, "ш": 26, "щ": 27, "ъ": 28,
-             "ы": 29, "ь": 30, "э": 31, "ю": 32, "я": 32
-             }
-    # меняем местами ключ и значение, такой словарь понадобится в будущем
-    dict2 = {v: k for k, v in dicts.items()}
-    text = input("Введите текст для шифрования").lower()
-    gamma = input("Введите гамму(на русском языке! Да и пробелы тоже нельзя! Короче, только символы из dict").lower()
-    listofdigitsoftext = list()  # сюда будем записывать числа букв из текста
-    listofdigitsofgamma = list()  # для гаммы
-    # запишем числа в список
-    for i in text:
-        listofdigitsoftext.append(dicts[i])
-    print("Числа текста", listofdigitsoftext)
-    # то же самое сделаем с гаммой
-    for i in gamma:
-        listofdigitsofgamma.append(dicts[i])
-    print("числа гаммы", listofdigitsofgamma)
-    listofdigitsresult = list()  # сюда будем записывать результат
-    ch = 0
-    for i in text:
-        try:
-            a = dicts[i] + listofdigitsofgamma[ch]
-        except:
-            ch = 0
-            a = dicts[i] + listofdigitsofgamma[ch]
-        if a >= 33:
-            a = a % 33
-        ch += 1
-        listofdigitsresult.append(a)
-    print("Числа зашифрованного текста", listofdigitsresult)
-    # теперь обратно числа представим в виде букв
-    textencrypted = ""
-    for i in listofdigitsresult:
-        textencrypted += dict2[i]
-    print("Зашифрованный текст: ", textencrypted)
-    # теперь приступим к реализации алгоритма дешифровки
-    listofdigits = list()
-    for i in textencrypted:
-        listofdigits.append(dicts[i])
-    ch = 0
-    listofdigits1 = list()
-    for i in listofdigits:
-        try:
-            a = i - listofdigitsofgamma[ch]
-        except:
-            ch=0
-            a = i - listofdigitsofgamma[ch]
-        if a < 1:
-            a = 33 + a
-        listofdigits1.append(a)
-        ch += 1
-    textdecrypted = ""
-    for i in listofdigits1:
-        textdecrypted += dict2[i]
-    print("Расшифрованный текст", textdecrypted)
+    # положим числа в переменные
+    a = int(input("Введите числа a"))
+    b = int(input("Введите число b"))
+    if a >= 0 and 0 <= b <= a: # проверяем условия что все в порядке(согласно условиям задачи
+        print("Вызываем функицю Евклида")
+        print(evklidsimply(a,b)) # вызываем функцию простого евклида
+        print("А теперь можно вызвать функцию расширенного")
+        print(evklid_extended(a,b)) # вызываем функцию расширенного евклида
+        print("А теперь функция бинарного Евклида")
+        print(binary_evklid(a,b)) # вызываем функцию бинарного евклида
+        print("А теперь функция расширенного бинарного Евклида")
+        print(evklid_binary_extended(a,b)) # вызываем функцию расширенного бинарного евклида
 
 
 if __name__ == '__main__':
